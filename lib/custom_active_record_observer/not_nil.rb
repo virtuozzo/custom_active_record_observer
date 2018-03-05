2c70a7d8dddb6f96778adcb3ddef5b526afab029
Volodymyr Byno <volodymyr.byno@gmail.com>
2018-03-05 12:48:10 +0200

    Provide a documentation with YARD
    Document DSL and NotNil modules

--- lib/custom_active_record_observer/not_nil.rb
+++ lib/custom_active_record_observer/not_nil.rb
@@ -1,5 +1,9 @@
+# @title CustomActiveRecordObserver::NotNil
 module CustomActiveRecordObserver
+  # A module to represent any of values that are not equal to +nil+
   module NotNil
+    # @param other [Any]
+    # @return [Boolean]
     def self.===(other)
       super || !other.nil?
     end
--- lib/custom_active_record_observer/observable/base.rb
+++ lib/custom_active_record_observer/observable/base.rb
@@ -1,5 +1,8 @@
 module CustomActiveRecordObserver
   module Observable
+    # The base module which is included to all classes that are observed.
+    # It adds AR after commit callbacks to class instances and handles
+    # the results with <tt>CustomActiveRecordObserver::Handler</tt>
     module Base
       extend ActiveSupport::Concern
 
